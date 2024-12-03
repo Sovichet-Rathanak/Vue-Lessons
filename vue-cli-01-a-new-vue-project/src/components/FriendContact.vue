de2b3b258abbc0e060beccd5404cb72ba22fbf3a
Sovichet Rathanak <rathanaksovichet@gmail.com>
2024-12-03 18:57:45 +0700

    a deeper dive into Vue project

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h1>{{ friend.name }}</h1>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide':'Show'}} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                detailsAreVisible: false,
+                friend:{
+                        id: 'manuel',
+                        name: 'Manuel Lorenz',
+                        phone: '012231 32321',
+                        email: 'manuel@gmail.com'
+                }
+            }
+        },
+        methods:{
+            toggleDetails(){
+                this.detailsAreVisible = !this.detailsAreVisible;
+            }
+        }
+    };
+</script>
